e71ef4958a213ee15a3efd603f5bb0112aebca51
PiotrJander <pjander2@gmail.com>
2017-05-31 15:16:43 +0200

    Refactors, cleans up, add dynamic automata.

--- pjk9048.pl
+++ pjk9048.pl
@@ -1,31 +1,50 @@
 % Piotr Jander
 
 % TODO
-% one grammar at a time
-% maybe let me model the parsing table first, and do the parsing
-% Automaton has states transitions
-% AutomatonRun has the stack additionally
-% table is static, stack changes
+% TODO we'd like debug messages when things dont go as expected
 
-action(State, Terminal, NewState).
-goto(State, Nonterminal, NewState).
-reduce(State, Number, Nonterminal).
+% shift(State, Terminal, NewState).
+% reduce(State, Number, Nonterminal).
+% goto(State, Nonterminal, NewState).
 
-% so the automaton (parsing table) is essentially a set of those relations
-% make it simple: hardcode those relations for our simple grammar
-% acceptance by empty stack? or by accept state
+:- dynamic shift_in/3, reduce_in/3, goto_in/3.
 
-% but it the reduce, we need to remember how many symbols to pop off the stack
+example_automaton([
+    shift_in(0, a, 3),
+    shift_in(0, b, 4),
+    shift_in(1, eof, accept),
+    shift_in(2, a, 3),
+    shift_in(2, b, 4),
+    shift_in(3, a, 3),
+    shift_in(3, b, 4),
+
+    reduce_in(4, 1, a),
+    reduce_in(5, 2, s),
+    reduce_in(6, 2, a),
+
+    goto_in(0, a, 2),
+    goto_in(0, s, 1),
+    goto_in(2, a, 5),
+    goto_in(3, a, 6).
+]).
 
 shift_in(0, a, 3).
 shift_in(0, b, 4).
-% shift_in()  % TODO accept
+shift_in(1, eof, accept).
 shift_in(2, a, 3).
 shift_in(2, b, 4).
 shift_in(3, a, 3).
 shift_in(3, b, 4).
 
 shift(state(From), t(Through), state(To)) :- shift_in(From, Through, To).
+
+reduce_in(4, 1, a).
+reduce_in(5, 2, s).
+reduce_in(6, 2, a).
+
+reduce(state(S), SuccNumber, nt(Nonterminal)) :-
+    reduce_in(S, Number, Nonterminal),
+    succs(Number, SuccNumber).
 
 goto_in(0, a, 2).
 goto_in(0, s, 1).
@@ -34,24 +53,24 @@ goto_in(3, a, 6).
 
 goto(state(From), nt(Through), state(To)) :- goto_in(From, Through, To).
 
-reduce_in(4, 1, a).
-reduce_in(5, 2, s).
-reduce_in(6, 2, a).
+% accept
 
-succs(0, 0).
-succs(N, succ(S)) :-
-    M is N - 1,
-    succs(M, S).
+assertall([]).
+assertall([Term|Database]) :-
+    assert(Term),
+    assertall(Database).
 
-reduce(state(S), SuccNumber, nt(Nonterminal)) :-
-    reduce_in(S, Number, Nonterminal),
-    succs(Number, SuccNumber).
+retract_automaton :-
+    retractall(shift_in(_, _, _)),
+    retractall(reduce_in(_, _, _)),
+    retractall(goto_in(_, _, _)).
 
-% accept(Automaton, Word).
+accept(Automaton, Word) :-
+    assertall(Automaton),
+    automaton(Word, [state(0)]),
+    retract_automaton.
 
-accept(Word) :- automaton(Word, [state(0)]).
-
-automaton([], Stack).  % TODO
+automaton([], [state(accept)]).
 automaton([Terminal|Word], [State|Stack]) :-  % we'd like the invariant that State is top of the Stack
     shift(State, Terminal, NewState),  % if we have a shift action
     automaton(Word, [NewState, Terminal, State | Stack])
@@ -66,7 +85,10 @@ automaton([Terminal|Word], [State|Stack]) :-  % we'd like the invariant that Sta
 stack_reduce([State|Stack], 0, Nonterminal, [NewState,Nonterminal,State|Stack]) :- goto(State, Nonterminal, NewState).
 stack_reduce([_,_|Stack], succ(Number), Nonterminal, Stack).
 
-% TODO we'd like debug messages when things dont go as expected
-% TODO accept by empty stack or by accept state
+%%%%%%%%%
+% helpers
 
-% TODO how do we accept empty string??
+succs(0, 0).
+succs(N, succ(S)) :-
+    M is N - 1,
+    succs(M, S).
